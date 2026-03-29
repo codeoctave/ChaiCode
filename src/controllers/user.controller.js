7818bc11dd39edd59b35fe1d4729a5f71062a7e3
codeoctave <vikasmishra360@live.in>
2026-03-29 09:14:45 +0530

    add bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -25,8 +25,13 @@ const generateAccessAndRefreshTokens = async (userId) => {
 
 const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, username, password } = req.body;
-  console.log("email: ", email);
 
+  //console.log("fullName: ", fullName)
+  //console.log("email: ", email)
+  //console.log("username: ", username)
+  //console.log("password: ", password)
+  //console.log("req.body: ", req.body)
+  
   if (
     [fullName, email, username, password].some((field) => field?.trim() === "")
   ) {
@@ -42,7 +47,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  //const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
   let coverImageLocalPath;
   if (
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -10,13 +10,16 @@ cloudinary.config({
 const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return null;
+
     //upload the file on cloudinary
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
+
     //file has been uploaded successfully
     // console.log("file is uploaded on cloudinary", response.url);
-    fs.unlinkSync(localFilePath)
+
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); //remove the locally saved temporary file as upload operation has failed.
